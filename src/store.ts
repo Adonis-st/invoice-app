421ef430dffe97a4031a13f005e025c342ae81f0
Adonis-st <adonisy851@gmail.com>
2025-06-14 11:00:22 -0500

    Start fresh rewrite

--- src/store.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from "jotai";
-import { atomWithStorage } from "jotai/utils";
-
-export const modalAtom = atom(false);
-
-export const datePickerAtom = atom(false);
-
-export const darKModeAtom = atomWithStorage("darkMode", false);
--- src/utils/formateDate.ts
+++
@@ -1,8 +0,0 @@
-export const formateDate = (newDate: string | Date) => {
-  const formattedDate = newDate.toLocaleString("en-us", {
-    day: "2-digit",
-    month: "short",
-    year: "numeric",
-  });
-  return `${formattedDate}`;
-};
